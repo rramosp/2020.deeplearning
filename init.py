2f03c723dbe49b894357c12611cb9cdd1b7f5045
rramosp <rramospQunal.edu.co>
2019-11-06 20:27:41 -0500

    init

--- init.py
+++ init.py
@@ -2,18 +2,6 @@ course_id = '2020.deeplearning'
 github_repo = 'rramosp/%s'%course_id
 zip_file_url="https://github.com/%s/archive/master.zip"%github_repo
 
-def get_github_last_commit_date():
-    try:
-        import requests
-        import dateutil.parser
-        from datetime import timezone
-
-        r = requests.get('https://api.github.com/repos/%s/commits/master'%github_repo).json()
-        date = dateutil.parser.parse(r["commit"]["committer"]["date"]).astimezone(timezone.utc)
-        return date
-    except Exception as e:
-        return None
-
 def get_last_modif_date(localdir):
     try:
         import time, os, pytz
@@ -22,15 +10,13 @@ def get_last_modif_date(localdir):
         localtz = datetime.datetime.now(datetime.timezone(datetime.timedelta(0))).astimezone().tzinfo
         k = k.astimezone(localtz)
         return k
-    except Exception as e:
+    except Exception:
         return None
     
 import requests, zipfile, io, os, shutil
 def init(force_download=False):
-    ghdate = get_github_last_commit_date()
-    localdate = get_last_modif_date('.')    
-    if force_download or ghdate is None or localdate is None or ghdate>localdate:
-        print ("replacing local resources")
+    if force_download or not os.path.exists("local"):
+        print("replicating local resources")
         dirname = course_id+"-master/"
         if os.path.exists(dirname):
             shutil.rmtree(dirname)
